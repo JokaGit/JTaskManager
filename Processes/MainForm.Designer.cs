f97ca05caeb3fe1da08f9630a3d344f0f0a81ee9
Joka <evgen_jr@mail.ru>
2020-06-15 16:20:53 +0300

    Add project files.

---
+++ Processes/MainForm.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace JTaskManager
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            this.btnKill = new System.Windows.Forms.Button();
+            this.btnReload = new System.Windows.Forms.Button();
+            this.btnGetPorts = new System.Windows.Forms.Button();
+            this.ColID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColApp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColPriority = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColMemory = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColPorts = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.btnClose);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(860, 76);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(79, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "JTaskManager";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::JTaskManager.Properties.Resources.Tasks;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(76, 76);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Image = global::JTaskManager.Properties.Resources.Close;
+            this.btnClose.Location = new System.Drawing.Point(784, 0);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(76, 76);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dgv);
+            this.panel2.Location = new System.Drawing.Point(19, 76);
+            this.panel2.Margin = new System.Windows.Forms.Padding(0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(841, 394);
+            this.panel2.TabIndex = 0;
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.AllowUserToResizeColumns = false;
+            this.dgv.AllowUserToResizeRows = false;
+            this.dgv.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgv.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgv.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColID,
+            this.ColName,
+            this.ColApp,
+            this.ColPriority,
+            this.ColMemory,
+            this.ColPorts});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.ActiveCaption;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgv.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgv.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv.Location = new System.Drawing.Point(0, 0);
+            this.dgv.Margin = new System.Windows.Forms.Padding(0);
+            this.dgv.MultiSelect = false;
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.RowHeadersVisible = false;
+            this.dgv.Size = new System.Drawing.Size(841, 394);
+            this.dgv.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.tbSearch);
+            this.panel3.Controls.Add(this.btnKill);
+            this.panel3.Controls.Add(this.btnReload);
+            this.panel3.Controls.Add(this.btnGetPorts);
+            this.panel3.Location = new System.Drawing.Point(0, 470);
+            this.panel3.Margin = new System.Windows.Forms.Padding(0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(860, 70);
+            this.panel3.TabIndex = 0;
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbSearch.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbSearch.Location = new System.Drawing.Point(69, 14);
+            this.tbSearch.Multiline = true;
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(273, 43);
+            this.tbSearch.TabIndex = 1;
+            this.tbSearch.TextChanged += new System.EventHandler(this.tbSearch_TextChanged);
+            // 
+            // btnKill
+            // 
+            this.btnKill.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnKill.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnKill.Location = new System.Drawing.Point(688, 14);
+            this.btnKill.Margin = new System.Windows.Forms.Padding(0);
+            this.btnKill.Name = "btnKill";
+            this.btnKill.Size = new System.Drawing.Size(152, 43);
+            this.btnKill.TabIndex = 0;
+            this.btnKill.Text = "Kill";
+            this.btnKill.UseVisualStyleBackColor = true;
+            this.btnKill.Click += new System.EventHandler(this.btnKill_Click);
+            // 
+            // btnReload
+            // 
+            this.btnReload.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReload.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnReload.Location = new System.Drawing.Point(356, 14);
+            this.btnReload.Margin = new System.Windows.Forms.Padding(0);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(152, 43);
+            this.btnReload.TabIndex = 0;
+            this.btnReload.Text = "Reload";
+            this.btnReload.UseVisualStyleBackColor = true;
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+            // 
+            // btnGetPorts
+            // 
+            this.btnGetPorts.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGetPorts.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnGetPorts.Location = new System.Drawing.Point(522, 14);
+            this.btnGetPorts.Margin = new System.Windows.Forms.Padding(0);
+            this.btnGetPorts.Name = "btnGetPorts";
+            this.btnGetPorts.Size = new System.Drawing.Size(152, 43);
+            this.btnGetPorts.TabIndex = 0;
+            this.btnGetPorts.Text = "Get Ports";
+            this.btnGetPorts.UseVisualStyleBackColor = true;
+            this.btnGetPorts.Click += new System.EventHandler(this.btnGetPorts_Click);
+            // 
+            // ColID
+            // 
+            this.ColID.FillWeight = 53.52945F;
+            this.ColID.HeaderText = "ID";
+            this.ColID.Name = "ColID";
+            this.ColID.ReadOnly = true;
+            this.ColID.Width = 50;
+            // 
+            // ColName
+            // 
+            this.ColName.FillWeight = 133.4771F;
+            this.ColName.HeaderText = "Name";
+            this.ColName.Name = "ColName";
+            this.ColName.ReadOnly = true;
+            this.ColName.Width = 254;
+            // 
+            // ColApp
+            // 
+            this.ColApp.FillWeight = 115.5824F;
+            this.ColApp.HeaderText = "App";
+            this.ColApp.Name = "ColApp";
+            this.ColApp.ReadOnly = true;
+            this.ColApp.Width = 290;
+            // 
+            // ColPriority
+            // 
+            this.ColPriority.FillWeight = 30.45685F;
+            this.ColPriority.HeaderText = "Priority";
+            this.ColPriority.Name = "ColPriority";
+            this.ColPriority.ReadOnly = true;
+            this.ColPriority.Width = 70;
+            // 
+            // ColMemory
+            // 
+            this.ColMemory.FillWeight = 133.4771F;
+            this.ColMemory.HeaderText = "Memory";
+            this.ColMemory.Name = "ColMemory";
+            this.ColMemory.ReadOnly = true;
+            this.ColMemory.Width = 80;
+            // 
+            // ColPorts
+            // 
+            this.ColPorts.FillWeight = 133.4771F;
+            this.ColPorts.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ColPorts.HeaderText = "Ports";
+            this.ColPorts.Name = "ColPorts";
+            this.ColPorts.ReadOnly = true;
+            this.ColPorts.Width = 80;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(860, 540);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JTaskManager";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnKill;
+        private System.Windows.Forms.Button btnGetPorts;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbSearch;
+        private System.Windows.Forms.Button btnReload;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColApp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColPriority;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColMemory;
+        private System.Windows.Forms.DataGridViewComboBoxColumn ColPorts;
+    }
+}
+
---
+++ Processes/MainForm.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace JTaskManager
+{
+    public partial class MainForm : Form
+    {
+        Process[] ProcessArray;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();        
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {            
+            LoadProcesses();
+        }
+
+        private void LoadProcesses()
+        {
+            dgv.Rows.Clear();
+            ProcessArray = Process.GetProcesses();
+            foreach (Process pr in ProcessArray)
+            {              
+                dgv.Rows.Add(pr.Id, pr.ProcessName, pr.MainWindowTitle, pr.BasePriority, pr.VirtualMemorySize64, "");
+            }
+            dgv.Sort(dgv.Columns[2], ListSortDirection.Descending);
+            dgv.ClearSelection();
+        }
+
+        private void btnReload_Click(object sender, EventArgs e)
+        {        
+            LoadProcesses();
+        }
+
+        private void btnKill_Click(object sender, EventArgs e)
+        {
+            if (dgv.SelectedCells.Count == 0)
+            {
+                MessageBox.Show("Выберите процесс");
+                return;
+            }
+            Process.GetProcessById(Int32.Parse(dgv.Rows[dgv.SelectedCells[0].RowIndex].Cells[0].Value.ToString())).Kill();
+            LoadProcesses();
+        }
+
+        private void tbSearch_TextChanged(object sender, EventArgs e)
+        {
+            LoadProcesses();
+
+            for (int i = 0; i < dgv.RowCount; i++)
+            {
+                if (!dgv.Rows[i].Cells[1].Value.ToString().ToLower().Contains(tbSearch.Text.ToLower()))
+                {
+                    dgv.Rows.RemoveAt(i);
+                    i--;
+                }
+            }
+        }
+
+        private void btnGetPorts_Click(object sender, EventArgs e)
+        {
+            string info = "";
+            foreach (ProcessPort p in ProcessPorts.ProcessPortMap.FindAll(x => x.ProcessId == Convert.ToInt32(dgv.CurrentRow.Cells[0].Value)))
+            {
+                info += (p.ProcessPortDescription) + "\n";
+            }
+            if (info == "")
+                MessageBox.Show("Ни один порт не используется");
+            else MessageBox.Show(info, $"Используемые процессом {dgv.CurrentRow.Cells[1].Value} порты");
+        }
+    }
+}
